1294b33633f2df6b6805d4d3069e62c8c15d1961
Moshe Reubinoff <Moshe.R@qualisystems.com>
2019-10-27 12:51:11 +0200

    test

--- src/__init__.py
+++ src/__init__.py
@@ -7,18 +7,6 @@ original_flask_handle_exception = app.handle_exception
 original_flask_handle_user_exception = app.handle_user_exception
 api = flask_restful.Api(app)
 
-### swagger specific ###
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
-    }
-)
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-### end swagger specific ###
 
 # modifies flask handle exception default behavior, but is implemented badly.
 # api = flask_restful.Api(app)
--- src/app.py
+++ src/app.py
@@ -1,2 +1,17 @@
 from . import app
 from .routes import errors, home
+
+
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Verion-Performance"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
