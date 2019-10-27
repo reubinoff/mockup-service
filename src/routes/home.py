445b6492b10a06c9e1f1c5cd8bb5c579af615827
Moshe Reubinoff <Moshe.R@qualisystems.com>
2019-10-27 12:40:11 +0200

    test

--- src/routes/home.py
+++ src/routes/home.py
@@ -1,6 +1,9 @@
+import time
+
 from flask import Flask, jsonify, Response
 from flask_restful import Resource, marshal_with, abort
 from .. import  api
+from .helpers import expect_json_data
 
 class Home(Resource):
 	def get(self):
@@ -22,5 +25,18 @@ class Perfecto(Resource):
         time.sleep(1)
         return jsonify({"status": True})
 
+class Robot(Resource):
+    def get(self):
+        time.sleep(1)
+        return jsonify({"status": True})
+    
+    @expect_json_data
+    def post(self, data):
+        time.sleep(2)
+        res = {"data": data, "status": True}
+        return jsonify(res)
+
+
 api.add_resource(Home, '/')
 api.add_resource(Perfecto, '/perfecto')
+api.add_resource(Robot, '/robot')
